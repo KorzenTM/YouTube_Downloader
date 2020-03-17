421f4e92232e0917a7b955d28ac9acc1e435bdbb
KorzenTM <mateuszkorzeniowski98@gmail.com>
2020-03-17 21:40:25 +0100

    Added set format

--- src/button_function.py
+++ src/button_function.py
@@ -1,7 +1,20 @@
 from importing_modules import *
 
 
-        
+def download_finished_window():
+    msg=QMessageBox()
+    msg.setWindowTitle("Informacja")
+    msg.setText("Pobieranie zostało zakończone")
+    msg.setIcon(QMessageBox.Information)
+    x=msg.exec_()
+    
+def convert_finished_window():
+    msg=QMessageBox()
+    msg.setWindowTitle("Informacja")
+    msg.setText("Konwersja została zakończona")
+    msg.setIcon(QMessageBox.Information)
+    x=msg.exec_()
+     
 def download_video(self,location,link):
     if self.link_textbox.text()=="":
         msg=QMessageBox()
@@ -16,30 +29,27 @@ def download_video(self,location,link):
         msg.setIcon(QMessageBox.Critical)
         x=msg.exec_()
     else:
-        def percent(tem,total):
-            perc=(float(tem)/float(total))*float(100)
-            return perc 
-
         def progress_function(chunk,file_handle,bytes_remaining):
             progres=round((1-bytes_remaining/self.video.filesize)*100, 3)
             self.status.setValue(progres)
-        
-        self.video_url =link # paste here your Youube videos' url
+            
+        self.video_url =link 
         self.youtube = pytube.YouTube(self.video_url,on_progress_callback=progress_function)
-        self.video = self.youtube.streams.first()
-        self.video.download(location)
+        self.video = self.youtube.streams.get_by_itag(self.set_format.itemData(self.set_format.currentIndex()))
         
-        
-        
-        msg=QMessageBox()
-        msg.setWindowTitle("Informacja")
-        msg.setText("Pobieranie zostało zakończone")
-        msg.setIcon(QMessageBox.Information)
-        x=msg.exec_()
+        if self.convert_mp3_check.isChecked():
+            self.video.download(location)
+            for file in[n for n in os.listdir(location) if re.search('mp4',n)]:
+                full_path=os.path.join(location,file)
+                output_path=os.path.join(location,os.path.splitext(file)[0]+'.mp3')
+                clip=mp.AudioFileClip(full_path)
+                clip.write_audiofile(output_path)
+            convert_finished_window();
+        else:
+            self.video.download(location)
+            download_finished_window()
          
 def open_directory(self,location):
-    
-    #print(self.save_location.text())
     webbrowser.open(location)
 
 def save_directory(self):
@@ -47,14 +57,24 @@ def save_directory(self):
     dialog.setOption(QFileDialog.ShowDirsOnly, True)
     dialog.setWindowTitle("Wybierz folder")
     dialog.setAcceptMode(QFileDialog.AcceptOpen)
-    #dialog.setNameFilter(nameFilter)
     dialog.setFileMode(QFileDialog.DirectoryOnly)
     if dialog.exec_() == QFileDialog.Accepted:
         self.save_location.setText(dialog.selectedFiles()[0])
 
 def open_link(self,link):
-    # print(self.link_textbox.text())
-    webbrowser.open(link)
+    self.video_url =link # paste here your Youube videos' url
+    self.youtube = pytube.YouTube(self.video_url)
+    self.video = self.youtube.streams.filter(progressive=True, file_extension='mp4').order_by('resolution')
+    self.description.setText("Tytuł:%s\nAutor:%s\nOpis:%s" %(self.youtube.title,self.youtube.author,self.youtube.description))
+    tag = ""
+    for i in self.video:
+        tag = re.split(r" itag=\"|\" mime_type", str(i))
+        temp=(str(i).split()[2].split("=")[1].split("/")[1]+" "+str(i).split()[3].split("=")[1]).replace('"',"").upper()
+        self.set_format.addItem(temp,int(tag[1]))
+        
+    
+        
+    
 
 def center(self):
         #geometry of the main window
